6bc26ffed590e2f43204e43d494e3cac83d606f8
ImranAoi <ansarimran987@gmail>
2025-03-23 02:09:43 +0530

    3rd

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
 const app = express();
+const cors = require("cors");
 
-const port = process.env.PORT || 5001;
+const mongoose = require("mongoose");
+const port = process.env.PORT || 5000;
+require('dotenv').config()
 
-// Middleware to parse JSON bodies
+// middleware
 app.use(express.json());
-
-// CORS configuration
 app.use(cors({
-    origin: [
-        'http://localhost:5173',
-        'https://new-book-store-lemon.vercel.app',
-        'https://book-store-bk-beta.vercel.app' // Add your backend domain too
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
     credentials: true
-}));
+}))
 
-// Routes
-
-
+// routes
 const bookRoutes = require('./src/books/book.route');
 const orderRoutes = require("./src/orders/order.route")
 const userRoutes =  require("./src/users/user.route")
-
+const adminRoutes = require("./src/stats/admin.stats")
 
 app.use("/api/books", bookRoutes)
 app.use("/api/orders", orderRoutes)
 app.use("/api/auth", userRoutes)
+app.use("/api/admin", adminRoutes)
 
 async function main() {
-    await mongoose.connect(process.env.DB_URL);
-    app.use('/', (req, res) => {
-        res.send('Book store server is running');
-    });
+  await mongoose.connect(process.env.DB_URL);
+  app.use("/", (req, res) => {
+    res.send("Book Store Server is running!");
+  });
 }
 
-main()
-    .then(() => console.log('MongoDB connect successful'))
-    .catch(err => console.log(err));
+main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
